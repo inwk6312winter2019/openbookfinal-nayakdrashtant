af0bc2ded80b9f7e6215034e25541574c3094087
Drashtant <drahtantnayak@dal.ca>
2019-02-15 15:14:35 -0400

    task03 Create file to get ticket

---
+++ task03.py
@@ -0,0 +1,14 @@
+import requests
+import json
+def geticket():
+    url = "https://sandboxapicem.cisco.com/api/v1/ticket"
+    data = {}
+    data["username"] = "devnetuser"
+    data["password"] = "Cisco123!"
+    data = json.dumps(data)
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=data,headers=headers)
+    print(response.json()['response']['serviceTicket'])
+
+geticket()
+
